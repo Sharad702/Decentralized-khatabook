d93f2201c3dc8900b7494947f5b9aaf35bbaec01
sharadgumber0702@gmail.com <your-email@example.com>
2025-04-26 16:27:35 +0530

    Removed tailwind config

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,9 @@
+// const tailwindcss = require('@tailwindcss/postcss');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-} 
+  plugins: [
+    // Remove Tailwind CSS from the plugins list
+    // tailwindcss,
+    require('autoprefixer'),
+  ],
+}; 
